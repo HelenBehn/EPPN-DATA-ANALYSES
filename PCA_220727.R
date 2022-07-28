164f06890db1f068e415cd599c087c201e19b1b1
HB <hbehn@uni-bonn.de>
2022-07-28 17:50:06 +0200

    nach hefin rhys, Problem: PCA-Ergebnis-Tabelle und Datensatz, in dem die Genotypen und Accessions sind, haben unterschiedlich viele Zeilen. Wenn ich Faktor-Variablen entferne (Genotyp und Accession), gehts. Dann fehlt diese Information aber, um die Diagramme danach zu färben.

--- PCA_220727.R
+++ PCA_220727.R
@@ -22,24 +22,9 @@ View(DATA)
 install.packages("factoextra")
 library(factoextra)
 
-#recode first column as row names ##
-remove_rownames(DATA) %>% has_rownames()
-DATA_PCA <- column_to_rownames(DATA, var = "Genotype")
-View(DATA_PCA)
-#
-rownames(DATA) <- DATA[,1]
-DATA[,1] <- NULL
-str(DATA)
-#
-DATA2 <- DATA[,-1]
-rownames(DATA2) <- DATA[,1]
-View(DATA2)
-# Geht nicht, offenbar weil nicht mehrere Zeilen denselben Namen haben dürfen##
-
-
 # Compute PCA - with rownames instead of factor column ### nachSTHDA ###
 # na.omit only works, if you apply it directly to the dataframe
-PCA_results <- prcomp(na.omit(DATA2), center = TRUE, scale = TRUE) 
+PCA_results <- prcomp(na.omit(DATA[ ,-1]), center = TRUE, scale = TRUE) 
 print(PCA_results)                                                                             
 
 # Visualize eigenvalues (scree plot). Show the percentage of variances 
@@ -55,6 +40,7 @@ fviz_pca_ind(PCA_results)
 PCA_results <- prcomp(na.omit(DATA[ ,-1]), center = TRUE, scale = TRUE)
 PCA_results
 
+
 summary(PCA_results)
 
 biplot(PCA_results)
@@ -67,6 +53,7 @@ View(PCA_results)
 PCA_results
 PCA_results$x 
 
+#cbind geht nicht, weil unterschiedliche anzahl Zeilen in Objekten
 DATA_pca <- cbind(DATA[ ,-1], PCA_results$x[,1:2])
 DATA_pca
 
@@ -75,3 +62,83 @@ library(ggplot2)
 ggplot(iris2, aes(PC1, PC2, col = Species, fill = Species)) +
   stat_ellipse(geom = "polygon", col = "black", alpha = 0.5) +
   geom_point(shape = 21, col = "black")
+
+
+
+##### https://cran.r-project.org/web/packages/ggfortify/vignettes/plot_pca.html##
+install.packages("ggfortify")
+library(ggfortify)
+
+#create column giving accessions/lines
+DATA_accession <- DATA %>%
+  mutate(Accession = case_when(
+    Genotype == "R1" ~ "B. rapa",
+    Genotype == "R2" ~ "B. rapa",
+    Genotype == "O1" ~ "B. oleracea",
+    Genotype == "O4" ~ "B. oleracea",
+    
+    Genotype == "R204" ~ "Resynth. B. napus", #ACHTUNG: in Rohdatentabelle "0" statt "O"
+    Genotype == "R103" ~ "Resynth. B. napus",
+    Genotype == "R102" ~ "Resynth. B. napus",
+    Genotype == "N1" ~ "Natural B. napus",
+    Genotype == "N4" ~ "Natural B. napus",
+    Genotype == "N5" ~ "Natural B. napus",
+    Genotype == "N6" ~ "Natural B. napus",
+    Genotype == "N7" ~ "Natural B. napus",
+    Genotype == "C1" ~ "B. carinata",
+    Genotype == "C2" ~ "B. carinata",
+    Genotype == "J1" ~ "B. juncea",
+    Genotype == "J2" ~ "B. juncea",
+    Genotype == "J3" ~ "B. juncea",
+    
+    Genotype == "J1C1" ~ "JC hybrid",
+    Genotype == "J1C2" ~ "JC hybrid",
+    Genotype == "O1J3_PH29" ~ "novel allohexaploid",
+    Genotype == "O1J3_PH47" ~ "novel allohexaploid",
+    Genotype == "N5C2J1_PH22" ~ "novel allohexaploid",
+    Genotype == "N5C2J2_PH23" ~ "novel allohexaploid",
+    Genotype == "N6C2J2_PH24" ~ "novel allohexaploid",
+    Genotype == "N1C1J1_PH25" ~ "novel allohexaploid",
+    Genotype == "N1C2J1_PH26" ~ "novel allohexaploid",
+    Genotype == "N4C2J1_PH27" ~ "novel allohexaploid",
+    Genotype == "N5C2J2_PH30" ~ "novel allohexaploid",
+    Genotype == "N1C1J1_PH31" ~ "novel allohexaploid",
+    Genotype == "N6C2J2_PH35" ~ "novel allohexaploid",
+    Genotype == "N1C1J1_PH36" ~ "novel allohexaploid",
+    Genotype == "N1C1J1_PH39" ~ "novel allohexaploid",
+    Genotype == "N1C2J1_PH40" ~ "novel allohexaploid",
+    Genotype == "N6C2J2_PH44" ~ "novel allohexaploid",
+    Genotype == "N4C2J1_PH45" ~ "novel allohexaploid",
+    Genotype == "N6C2J2_PH48" ~ "novel allohexaploid",
+    Genotype == "N7C1J1" ~ "novel allohexaploid",
+    
+    Genotype == "N1C1J1.N5C2J2" ~ "allohexaploid hybrid",
+    Genotype == "N1C1J1.N1C2J1" ~ "allohexaploid hybrid",
+    Genotype == "N5C2J2.N7C1J1" ~ "allohexaploid hybrid",
+    Genotype == "N5C2J2.N5C2J2" ~ "allohexaploid hybrid",
+    Genotype == "N1C1J1. N6C2J2" ~ "allohexaploid hybrid",
+    Genotype == "N5C2J2.N6C2J2" ~ "allohexaploid hybrid",
+    Genotype == "N6C2J2.O1J3" ~ "allohexaploid hybrid",
+    Genotype == "N6C2J2.N4C2J1" ~ "allohexaploid hybrid",
+    Genotype == "N6C2J2.N7C1J1" ~ "allohexaploid hybrid"
+  ))
+View(DATA_accession)
+
+#rownames
+remove_rownames(DATA_accession) %>% has_rownames()
+column_to_rownames(DATA_accession, var = "Accession")
+rownames(DATA_accession)
+
+#plot PCA
+autoplot(PCA_results)
+autoplot(PCA_results, data = DATA_accession, colour = 'Accession')
+autoplot(PCA_results, data = DATA_accession[,2:33], colour = 'Accession')
+
+autoplot(PCA_results, data = na.omit(DATA_accession[,2:33]), colour = 'Accession')
+autoplot(PCA_results, data = na.omit(DATA_accession[,2:33]))
+
+autoplot(prcomp(na.omit(DATA[ ,-1]), center = TRUE, scale = TRUE), data = na.omit(DATA_accession), colour = 'Accession')
+
+
+View(na.omit(DATA_accession))
+View(PCA_results$x)
